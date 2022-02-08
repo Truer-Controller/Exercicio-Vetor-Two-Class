c432fba805cdf8b3f069de63bb0540849804a8a5
Rafael Augusto <rafajunior2907@gmail.com>
2022-02-07 23:01:41 -0300

    Add project files.

---
+++ Exerciciovetor2/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerciciovetor2
+{
+    class Product
+    {
+        public string NameProduct { get; set; }
+        public double PriceProduct { get; set; }
+    }
+}
---
+++ Exerciciovetor2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerciciovetor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberChoose = int.Parse(Console.ReadLine());
+
+            Product[] productVetor = new Product[numberChoose];
+
+            for(int i = 0; i < numberChoose; i++)
+            {
+                string nameproduct = Console.ReadLine();
+                double priceproduct = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                productVetor[i] = new Product { NameProduct = nameproduct, PriceProduct = priceproduct };
+            }
+
+            double sum = 0.0;
+            for(int i = 0; i < numberChoose; i++)
+            {
+                sum += productVetor[i].PriceProduct;
+            }
+
+            double avaregePrice = sum / numberChoose;
+            Console.WriteLine("Avarege product price: " + avaregePrice.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
